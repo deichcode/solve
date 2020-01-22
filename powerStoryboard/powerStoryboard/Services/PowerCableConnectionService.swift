5c987599277300d3fd46aa97e3f710a09f381ccd
Sören Schröder <code@razupaltuff.eu>
2020-01-22 11:30:33 +0100

    Add maze

--- powerStoryboard/powerStoryboard/Services/PowerCableConnectionService.swift
+++ powerStoryboard/powerStoryboard/Services/PowerCableConnectionService.swift
@@ -8,11 +8,14 @@
 import UIKit
 
 protocol PowerCableConnectionServiceProtocol {
+    
     func register(connectCallback:  @escaping () -> ()) -> Void
     func register(disconnectCallback:  @escaping () -> ()) -> Void
+    func isPluggedIn() -> Bool
 }
 
 class PowerCableConnectionService : PowerCableConnectionServiceProtocol {
+    
     private var batteryState: UIDevice.BatteryState { UIDevice.current.batteryState }
     private var connectCallback : (() -> ())?
     private var disconnectCallback : (() -> ())?
@@ -23,11 +26,24 @@ class PowerCableConnectionService : PowerCableConnectionServiceProtocol {
         enableBatteryMonitoring()
     }
     
+    func register(connectCallback: @escaping () -> ()) {
+        self.connectCallback = connectCallback
+        propagateBatteryState()
+    }
+    
+    func register(disconnectCallback: @escaping () -> ()) {
+        self.disconnectCallback = disconnectCallback
+        propagateBatteryState()
+    }
+    
+    func isPluggedIn() -> Bool {
+        return batteryState == .charging || batteryState == .full
+    }
+    
     fileprivate func enableBatteryMonitoring() {
         UIDevice.current.isBatteryMonitoringEnabled = true
         NotificationCenter.default.addObserver(self, selector: #selector(batteryStateDidChangeCallback), name: UIDevice.batteryStateDidChangeNotification, object: nil)
     }
-    
     
     @objc private func batteryStateDidChangeCallback(_ notification: Notification) {
         propagateBatteryState()
@@ -45,15 +61,5 @@ class PowerCableConnectionService : PowerCableConnectionServiceProtocol {
             guard let callback = disconnectCallback else { return }
             callback()
         }
-    }
-    
-    func register(connectCallback: @escaping () -> ()) {
-        self.connectCallback = connectCallback
-        propagateBatteryState()
-    }
-    
-    func register(disconnectCallback: @escaping () -> ()) {
-        self.disconnectCallback = disconnectCallback
-        propagateBatteryState()
     }
 }
