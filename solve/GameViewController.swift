20bc01d29c4abbb34d16b14dc01c1165a24ba67b
Sören Schröder <code@razupaltuff.eu>
2020-01-22 11:44:22 +0100

    Add project for final solve game

---
+++ solve/GameViewController.swift
@@ -0,0 +1,46 @@
+//
+//  GameViewController.swift
+//  Solve
+//
+//  Created by Sören Schröder on 22.01.20.
+//  Copyright © 2020 Sören Schröder. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return false
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscapeRight
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
